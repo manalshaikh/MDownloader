edc30a9d1be51b8cd391a9be6d4eee31ecb6fd30
SomilGumber <somilgumber05@gmail.com>
2020-06-16 22:09:45 +0530

    Added files

---
+++ app/web_scrapper.py
@@ -0,0 +1,35 @@
+import requests
+import io
+import re
+
+def grab(string, start, end):
+    match = re.search(r'%s[^<]*%s' % (start, end), string)
+    if match:
+        return match.group().split(start)[1][:-len(end)]
+    else:
+        return False
+
+url = []
+
+file = open("url.txt", "r")
+
+for i in file:
+    i = i.replace("\n", "")
+    url.append(i)
+
+for i in url:
+    # String - Start - End
+    # Example filename = grab(i, 'geos/', '.html') 
+    # It will be grab the word between that goes/ and html
+    start = 'geos/' 
+    end = '.html'
+    filename = grab(i, start, end) 
+    res = requests.get(i)
+    type(res)
+    fname = filename + ".html"
+    fname = "data/" + fname
+    with io.open(fname, "w", encoding="utf-8") as f:
+        f.write(res.text)
+        f.close()
+        print(filename + ".html is downloaded.")
+print("All file(s) are downloaded!")
